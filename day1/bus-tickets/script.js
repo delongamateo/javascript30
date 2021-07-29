a6d3f74001bc2c7f06be3949f92f7a906074b4cb
delongamateo <delongamateo@gmail.com>
2021-07-29 19:04:14 +0200

    comments

--- day1/bus-tickets/script.js
+++ day1/bus-tickets/script.js
@@ -6,19 +6,25 @@ const display = document.querySelector(".price");
 function calculate() {
   const regularPrice = 12;
   // logic calculating price based on age
+  // if age is below 6 ticket is free
   if (input.value < 6) {
     return (display.innerHTML = "Free");
+  // if age is beetwene  6 and 15 you get 35% discount
   } else if (input.value >= 6 && input.value <= 15) {
     return (display.innerHTML = `${Math.floor(
       regularPrice - regularPrice * 0.35
     )}$`);
+  // if age is beetwene  16 and 26 you get 65% discount
   } else if (input.value >= 16 && input.value <= 26) {
     return (display.innerHTML = `${Math.floor(
       regularPrice - regularPrice * 0.65
     )}$`);
+  // if age is beetwene  27 and 64 you pay full price
   } else if (input.value >= 27 && input.value <= 64) {
     return (display.innerHTML = `${regularPrice}$`);
-  } else {
+  }
+  // if age is 65 and above you pay half price
+  else {
     return (display.innerHTML = `${regularPrice / 2}$`);
   }
 }
